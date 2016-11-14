baa25d1e7704100df3448c0b77d196cb9b0aeede
lionel <lisi_xdu@126.com>
2016-11-14 16:25:12 +0800

    add some implements

--- src/main/testApp/Test.java
+++ src/main/testApp/Test.java
@@ -1,24 +1,28 @@
-package TestApp;
-
+package testApp;
 
 import accessControl.Student;
 
 public class Test {
 	public static void main(String[] args) {
-		Student qiang=new Student("XiaoQiang");		
+		Student qiang = new Student("XiaoQiang");
 		qiang.showInfo();
-		Student jun=new NewStudent("xiaoJun");
+		Student jun = new NewStudent("xiaoJun");
 		jun.showInfo();
 	}
 
-	
 }
-class NewStudent extends Student{
+/**
+ * @see Student
+ * @author Administrator
+ *
+ */
+class NewStudent extends Student {
 
 	public NewStudent(String name) {
 		super(name);
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
 	protected void setScore(int score) {
 		// TODO Auto-generated method stub
